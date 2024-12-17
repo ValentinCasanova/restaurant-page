951e9fb9409320acb1d848408b66aa89fed7041b
Valentin <vcasanov@ucsc.edu>
2024-12-17 11:02:54 -0800

    button eventHandler dynamically loads new html content

--- src/index.js
+++ src/index.js
@@ -12,14 +12,23 @@ content.appendChild(home);
 function changePage(nextPage) {
     if(nextPage == currPage) return;
     content.removeChild(content.children[0]);
+    currPage = nextPage;
+    let currLoad = '';
+    switch(currPage){
+        case 'home':
+            currLoad = home;
+            break;
+        case 'about':
+            currLoad = about;
+            break;
+        case 'menu':
+            currLoad = menu;
+            break;
+    }
+    content.appendChild(currLoad);
+    document.querySelector('body').style.backgroundColor = '#BFD2BF';
 }
 
 for(let button of buttons){
     button.addEventListener('click', () => changePage(button.innerText.toLowerCase()));
 }
-
-
-
-//loadMenu();
-
-//loadAbout();
